12150abd4d4d1de699ac4c070bd6390c31a96cb4
imartinovic <martinovic.igor94@gmail.com>
2018-12-27 18:25:56 +0100

    extracted windows for S03

---
+++ source/preprocessing/DAPHNET/clean_windows_personalized.m
@@ -0,0 +1,74 @@
+function [clean_gait,clean_fog] = clean_windows_personalized(input_file,...
+                                               labels_file, window_length,...
+                                               sample_rate, overlap_length)
+%CLEAN_WINDOWS_PERSONALIZED Summary of this function goes here
+%   Detailed explanation goes here
+input_path = strcat('../../../data/DAPHNET_txt_data/',input_file);
+labels_path = strcat('../../../data/DAPHNET_txt_data/',labels_file);
+raw_data = load(input_path);
+raw_intervals = floor(64*load(labels_path));
+clean_gait = [];
+clean_fog = [];
+num_samp = window_length * sample_rate;
+num_overlap = overlap_length * sample_rate;
+for it_in = 1:length(raw_intervals)
+   
+    if raw_intervals(it_in,1) ~= 0
+    
+        gait_data = raw_data(raw_intervals(it_in,1):raw_intervals(it_in,2),:);
+        % window iterator
+        it_w = int64(1);
+        % current sample at the start of the window
+        head = int64(1);
+        % number of windows
+        windows_count = int64(fix((length(gait_data)-num_overlap)/(num_samp-num_overlap)));
+        
+        while it_w <= windows_count    
+        
+            clean_gait = cat(3,clean_gait,gait_data(head:head+num_samp-1,:));
+            it_w = it_w + 1;
+            head = head + num_samp -1 - num_overlap;
+        end
+      
+    end
+    
+    if raw_intervals(it_in,3) ~= 0
+    
+        fog_data = raw_data(raw_intervals(it_in,2):raw_intervals(it_in,3),:);
+        % window iterator
+        it_w = int64(1);
+        % current sample at the start of the window
+        head = int64(1);
+        % number of windows
+        windows_count = int64(fix((length(fog_data)-num_overlap)/(num_samp-num_overlap)));
+        
+        while it_w <= windows_count    
+        
+            clean_fog = cat(3,clean_fog,fog_data(head:head+num_samp-1,:));
+            it_w = it_w + 1;
+            head = head + num_samp -1 - num_overlap;
+        end
+      
+    end
+    
+    
+    
+end
+
+clean_fog(:,1:10,:) = clean_fog(:,1:10,:)/1000;
+clean_gait(:,1:10,:) = clean_gait(:,1:10,:)/1000;
+
+output_data_path = '../../../data/DAPHNET_mat_files/windows/personalized/S03/';
+settings_file = load(strcat(output_data_path,'settings_id.mat'));
+settingsID = settings_file.settingsID;
+folder_name = strcat('settings',num2str(settingsID));
+mkdir(strcat(output_data_path,folder_name));
+% add log file with settings values
+settingsID = settingsID + 1;
+save(strcat(output_data_path,'settings_id.mat'),'settingsID');
+training_data = cat(3,clean_gait,clean_fog);
+save(strcat(output_data_path,folder_name,'/','training_data.mat'),'training_data');
+
+
+end
+
---
+++ source/preprocessing/DAPHNET/create_windows_personalized.m
@@ -0,0 +1,103 @@
+% creates sample windows for all sessions based on input parameters
+% labels each window, can assign additional info
+function [error_code] = create_windows(window_length, sample_rate, overlap_length)
+
+
+%% 
+% creating mat files
+
+% addpath /home/igor/lab_rotation/parkinsons_freezing_project/source/DAPHNET_code/scripts/
+% preprocess_DAPHNET;
+% rmpath /home/igor/lab_rotation/parkinsons_freezing_project/source/DAPHNET_code/scripts/
+%% 
+num_samp = window_length * sample_rate;
+num_overlap = overlap_length * sample_rate;
+input_path = '../../../data/DAPHNET_mat_files/';
+output_data_path = '../../../data/DAPHNET_mat_files/windows/personalized/S03/';
+files = dir(strcat(input_path,'*.mat'));
+total = length(files);
+elapsed = 0;
+% somehow identify current configuration of parameters
+settingsID = 105;
+% initializing training and test 3D matrices
+training_set = [];
+training_labels = [];
+test_set = [];
+test_labels = [];
+for file = files'
+    input_data_file = strcat(input_path,file.name);
+    create_windows_session(num_samp, num_overlap, ...
+                     input_data_file, output_data_path,file.name, settingsID);
+    % creating training and test set 3d window data matrices  
+
+    folder_name = strcat('length',num2str(settingsID));
+    folder_path = strcat(output_data_path,folder_name,'/');
+
+    output_data_file = strcat(folder_path,strcat('3D_',file.name));
+    labels_file = strcat(folder_path,strcat('labels_',file.name));
+    out_data_struct = load(output_data_file);
+    labels_struct = load(labels_file);
+    if (file.name == "S03R02.mat")
+        test_set = cat(3,test_set,out_data_struct.windows_3D);
+        test_labels = cat(1,test_labels, labels_struct.window_labels);
+    else 
+        if (file.name == "S03R01.mat")
+        training_set = cat(3,training_set,out_data_struct.windows_3D);
+        training_labels = cat(1,training_labels, labels_struct.window_labels);
+        end
+    end
+    clearvars out_data_struct
+    elapsed = elapsed + 1;
+    %clc
+    disp(strcat(num2str(100 * elapsed/total),' % elapsed'));
+end
+%clc
+disp('100 % elapsed');
+
+% saving training and test sets
+folder_name = strcat('length',num2str(settingsID));
+filename = strcat('training_set_',num2str(settingsID));
+name = strcat(output_data_path,'/',folder_name,'/',filename);
+save(name,'training_set');
+
+filename = strcat('test_set_',num2str(settingsID));
+name = strcat(output_data_path,folder_name,'/',filename);
+save(name,'test_set');
+
+% saving the labels
+filename = strcat('training_labels_',num2str(settingsID));
+name = strcat(output_data_path,folder_name,'/',filename);
+save(name,'training_labels');
+
+filename = strcat('test_labels_',num2str(settingsID));
+name = strcat(output_data_path,folder_name,'/',filename);
+save(name,'test_labels');
+
+error_code = 0;
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
